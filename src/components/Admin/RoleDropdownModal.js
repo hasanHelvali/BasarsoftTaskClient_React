8a619546a1c144b391de3aeba9e237afc3bc716c
HasanHelvali <hasan.helvali.git@gmail.com>
2024-05-08 11:39:58 +0300

    end

---
+++ src/components/Admin/RoleDropdownModal.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react'
+import { Dialog } from 'primereact/dialog';
+import { Dropdown } from 'primereact/dropdown';
+function RoleDropdownModal({defaultRole,rowData,handleClose,isUpdateRole,handleSave}) {
+  const [visible, setVisible] = useState(true);
+  const [selectedRole, setSelectedRole] = useState()
+  const [disabledButton, setdisabledButton] = useState(true)
+ 
+  const roles=[
+    { name: 'Admin',id:1},
+    { name: 'User' ,id:2},
+  ]
+ 
+  useEffect(() => {
+    setdisabledButton(true)
+    setSelectedRole(defaultRole)
+  }, [defaultRole])
+  
+  const getIdByRole=(id)=>{
+    // console.log(rowData.role[0]);
+    // const id=roles[rowData.role[0]]
+    // console.log(id);
+    // console.log(roles[0].name);
+    // for (let i = 0; i < roles.length; i++) {
+    //     if (roles[i].name === rowData.role[0]) {
+    //         setSelectedRole(roles[i].id)
+    //     //   return roles[i].id;
+    //       return roles[i].name;
+    //     }
+    // }
+    let name = roles.find((role)=>role.id===id).name
+    return name
+    debugger
+  }
+
+  const handleChange=(e)=>{
+    console.log(rowData);
+    let _role = getIdByRole(e.value)
+    console.log(defaultRole);
+    console.log(_role);
+
+    if(defaultRole[0]!==_role){
+      setdisabledButton(false)
+      setSelectedRole(_role)
+    }
+    else{
+      setdisabledButton(true)
+      setSelectedRole(defaultRole)
+
+    }
+  }
+  return (
+    <>
+
+    {/* <Dialog className='bg-light border d-flex justify-content-center  m-3 '   visible={visible} style={{ width: '10vw' ,"borderRadius":"5px","height":"20vh"}} onHide={() => setVisible(false)}> */}
+    <Dialog header="Role Guncelle" visible={isUpdateRole} position={"top"} style={{ width: '15vw',borderRadius:"5px", margin:"10px", padding:"10px", height:"200px" }}
+     onHide={() => {setSelectedRole(defaultRole); setdisabledButton(true); handleClose()}} footer={""} draggable={true} resizable={false} className='bg-white d-flex m-auto'
+    dismissableMask modal={true} maskStyle={{"backgroundColor":"rgba(0, 0, 0, 0.5)"}}>
+      <hr/>
+      <Dropdown
+      className='bg-light m-3 border w-75 justify-content-center'
+      panelClassName='bg-light mt-2'
+      value={selectedRole}
+      options={roles}
+      // onChange={(e) => handleChangeRole(e.value,rowData)}
+      onChange={(e) => handleChange(e)}
+      optionValue="id"
+      optionLabel="name"
+      placeholder={selectedRole}
+      // className={`w-full md:w-14rem drop${rowData.id}`}
+      />
+      <div className='m-auto'>
+        <button className='btn btn-danger ' disabled={disabledButton} onClick={()=>handleSave(selectedRole)}>Onayla</button>
+      </div>
+    </Dialog>
+      </>
+  )
+}
+
+export default RoleDropdownModal
--- src/components/Admin/Roles.js
+++ src/components/Admin/Roles.js
@@ -7,6 +7,7 @@ import { useMyContext } from '../../context/DataContext';
 import { sendRequest } from '../../services/httpClient.service';
 import { useNavigate } from 'react-router-dom';
 import { Users } from '../../models/User';
+import RoleDropdownModal from './RoleDropdownModal';
 // import Dropdown from 'react-bootstrap/Dropdown';
 
 function Roles() {
@@ -17,6 +18,8 @@ function Roles() {
     const [isRoleChanged, setisRoleChanged] = useState(false)
     const [dropdownStates, setDropdownStates] = useState({});
     const [approwedArr, setapprowedArr] = useState([])//approwedArr bir liste
+    const [isUpdateRole, setisUpdateRole] = useState(false)
+    const [defaulRole, setDefaultRole] = useState()
     const handleChangeRole = (selectedValue, rowData) => {
         // Yeni state'i oluştur
         const newDropdownStates = { ...dropdownStates };
@@ -72,19 +75,7 @@ function Roles() {
         { name: 'User' ,id:2},
       ]
 
-      const getIdByRole=(rowData)=>{
-        console.log(rowData.role[0]);
-        const id=roles[rowData.role[0]]
-        console.log(id);
-        console.log(roles[0].name);
-        for (let i = 0; i < roles.length; i++) {
-            if (roles[i].name === rowData.role[0]) {
-                setselectedRole(roles[i].id)
-            //   return roles[i].id;
-              return roles[i].name;
-            }
-        }
-      }
+
       const getRoleById=(id)=>{
         for (let i = 0; i < roles.length; i++) {
             if (roles[i].id === id) {
@@ -99,28 +90,38 @@ function Roles() {
       };
 
 
-      const handleSaveRole=(rowData)=>{
+      const handleSaveRole=(role)=>{
+        setisUpdateRole(false)
+        setrowData(null)
+        console.log(role);
+        console.log(rowData.id);
           handleLoading(true)
-        console.log(dropdownStates[rowData.id]);
+
+        // console.log(dropdownStates[rowData.id]);
         // console.log( getDefaultRole(rowData))
         // console.log(getIdByRole(rowData));
-        let role = getRoleById(dropdownStates[rowData.id])
-        const roleArr=[role]
+        // let role = getRoleById(dropdownStates[rowData.id])
+        // const roleArr=[role]
         const user=new Users(rowData.id,rowData.name,rowData.email,role)
         console.log(user);
         sendRequest("roles","UpdateRole","PUT",user).then((result)=>{
-            handleLoading(false)
-            alert("Guncelleme islemi basarılı")
-            getAllUser()
-        }).catch((err)=>{
-            handleLoading(false)
-            alert("Guncelleme islemi sırasında bir hata olustu.")
+          handleLoading(false)
+          alert("Guncelleme islemi basarılı")
+          getAllUser()
+      }).catch((err)=>{
+          handleLoading(false)
+          alert("Guncelleme islemi sırasında bir hata olustu.")
 
-        })
+      })
+
       }
       const createHandleMenuClick=()=>{
         
       }
+      const handleCloseRoleComponent=()=>{
+        setisUpdateRole(false)
+      }
+      const [rowData, setrowData] = useState()
   return (
     <>
     <div className="card"  style={{" height": "90vh"}}>
@@ -128,38 +129,20 @@ function Roles() {
         <Column field="id" header="ID"  ></Column> 
         <Column field="email" header="Email"  ></Column> 
         <Column field="role" header="Role"  ></Column> 
-        <Column  header="Role Düzenle"  body={(rowData)=> 
-            <div className="card flex justify-content-center">
-                <div className="dropdown-demo">
-                    <div className="card">
-                        <Dropdown
-                        value={dropdownStates}
-                        options={roles.filter(role=>role.name!==rowData.role[0])}
-                        onChange={(e) => handleChangeRole(e.value,rowData)}
-                        optionValue="id"
-                        optionLabel="name"
-                        placeholder="Select a Role"
-                        className={`w-full md:w-14rem drop${rowData.id}`}
-                        />
-                            {/* <Dropdown style={}>
-                              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                Dropdown Button
-                              </Dropdown.Toggle>
-
-                              <Dropdown.Menu>
-                                {roles.map((role)=>{
-                                  return <Dropdown.Item >role</Dropdown.Item>
-                                })}
-                              </Dropdown.Menu>
-                            </Dropdown> */}
-                    </div>
-                    </div>
-            </div> }>
+        <Column  header="Role Düzenle"  body={(rowData)=>(
+          <button className='btn btn-warning' onClick={()=>
+            {
+              setisUpdateRole(true)
+              setDefaultRole(rowData.role)
+              setrowData(rowData)
+            }}>Rol Seç</button>
+        ) }>
         </Column> 
-        <Column  header="Onayla" body={(rowData)=><button onClick={()=>{handleSaveRole(rowData)}} disabled={approwedArr.indexOf(rowData.id)<=-1} className={`btn btn-danger btn${rowData.id}`} >Onayla</button>}  ></Column> 
+        {/* <Column  header="Onayla" body={(rowData)=><button onClick={()=>{handleSaveRole(rowData)}} disabled={approwedArr.indexOf(rowData.id)<=-1} className={`btn btn-danger btn${rowData.id}`} >Onayla</button>}  ></Column>  */}
             {/*  approwedArr icinde olmayan id ler i kilitledik. */}
     </DataTable>
 </div>
+    {<RoleDropdownModal defaultRole={defaulRole} handleClose={handleCloseRoleComponent} isUpdateRole={isUpdateRole} rowData={rowData} handleSave={handleSaveRole}/>}
 </>
   )
 }
